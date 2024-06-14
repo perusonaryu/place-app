fbc803956f52e93ad3d8a83721ebb4054a285831
perusonaryu <perusonaryu1127@gmail.com>
2024-06-14 23:09:46 +0900

    色々

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import '../app.css'
+
+	$: pathname = $page.url.pathname
+</script>
+
+<div>
+	<nav>
+		<ul>
+			<li><a href="/" class:active={pathname === '/'}>Top</a></li>
+			<li><a href="/about" class:active={pathname === '/about'}>About</a></li>
+			<li><a href="/user" class:active={pathname === '/user'}>User List</a></li>
+		</ul>
+	</nav>
+	<slot></slot>
+</div>
+
+
+<style>
+	.active {
+		color: red;
+		font-weight: 900;
+	}
+</style>
