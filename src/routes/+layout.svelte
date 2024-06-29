dca425b86742453ff476b966c151fc02ae6aac0c
perusonaryu <perusonaryu1127@gmail.com>
2024-06-29 17:38:26 +0900

    footer作成

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,12 +3,15 @@
 </script>
 
 <div>
-  <header>
-    <h1 class="text-center text-[#AFDEEC] text-4xl mb-[100px] p-5">
+  <header class="mb-16">
+    <h1 class="text-center text-[#AFDEEC] text-4xl p-5">
       <a href="/">RyuBlog</a>
     </h1>
   </header>
-  <div class="max-w-[960px] m-auto">
+  <div class="max-w-[960px] m-auto mb-10">
     <slot></slot>
   </div>
+  <footer class="bg-[#393C3C] p-5">
+    <p class="text-xs text-white text-center">&copy; 2024 RyuBlog Inc.</p>
+  </footer>
 </div>
