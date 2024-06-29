e15dfeefa07fcbaff1631ac210aa73aae02f41dc
perusonaryu <perusonaryu1127@gmail.com>
2024-06-29 15:08:31 +0900

    投稿日を追加、文言修正修正

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -31,11 +31,13 @@
   <meta name="description" content="Svelte demo app" />
 </svelte:head>
 <div>
-  <div>{dayjs(data.updatedAt).format('YYYY-MM-DD')}</div>
+  <div class="flex gap-x-3 text-sm text-black/70 mb-2">
+    <span>投稿日：{dayjs(data.createdAt).format('YYYY年MM月DD日')}</span>
+    <span>最終更新日：{dayjs(data.updatedAt).format('YYYY年MM月DD日')}</span>
+  </div>
   <h1 class="text-3xl font-bold mb-6">{data.title}</h1>
   <div class="content">
     <!-- eslint-disable-next-line svelte/no-at-html-tags -->
     {@html data.content}
-    <p>ssssssssssss</p>
   </div>
 </div>
