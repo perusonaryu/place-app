1d6ceca3a22412a1ba7f1f6693b3b75ae6703dc5
perusonaryu <perusonaryu1127@gmail.com>
2024-06-29 17:46:13 +0900

    footerを下部に位置するよう修正

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,7 +11,7 @@
   <div class="max-w-[960px] m-auto mb-10">
     <slot></slot>
   </div>
-  <footer class="bg-[#393C3C] p-5">
+  <footer class="bg-[#393C3C] p-5 absolute bottom-0 w-full">
     <p class="text-xs text-white text-center">&copy; 2024 RyuBlog Inc.</p>
   </footer>
 </div>
