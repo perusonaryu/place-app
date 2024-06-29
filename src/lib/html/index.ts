e5ff0c765eda432ac3e42627091238847179cd6a
perusonaryu <perusonaryu1127@gmail.com>
2024-06-29 22:08:34 +0900

    html系の関数リファクタ

--- src/lib/html/index.ts
+++ src/lib/html/index.ts
@@ -2,7 +2,10 @@ import { load } from 'cheerio'
 import hljs from 'highlight.js'
 import 'highlight.js/styles/default.css' //
 
+// メソッド使用時は下記highlight用のcssの読み込みも必要
+// import 'highlight.js/styles/default.css'
 export function highlightCode(stringHtml: string) {
+  if (!stringHtml) return 'not stringHtml'
   const $ = load(stringHtml)
   $('code').each((_, elm) => {
     const result = hljs.highlightAuto($(elm).text())
@@ -14,6 +17,7 @@ export function highlightCode(stringHtml: string) {
 
 // ページのヘッダーに記載するためのディスクリプションを取得する
 export function getHeadDescription(stringHtml: string) {
+  if (!stringHtml) return 'not description'
   const $ = load(stringHtml)
   return $($('p')[0]).text()
 }
